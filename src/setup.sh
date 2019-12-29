dccfc0f910e3476963c486bc5eaf6bdf03e0a6af
Aaron Dutton <aaron.dutton@logos.com>
2019-12-28 16:01:07 -0800

    Add radio load/save tools

--- src/setup.sh
+++ src/setup.sh
@@ -7,3 +7,10 @@ then
     echo "##### Automatically written by setup.sh" >>.profile
     echo "sh /home/pi/run-jukebox.sh" >>.profile
 fi
+
+if [ ! -d moode-radio-utils ];
+then
+    git clone https://github.com/TheOldPresbyope/moode-radio-utils.git
+
+    echo "Y" | sudo python3 ./moode-radio-utils/loadmyradios.py
+fi
